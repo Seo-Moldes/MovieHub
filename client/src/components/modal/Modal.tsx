1309b42e733d2cdbfe5b9af263d4a4152aa002ae
Seo-Moldes <seomoldes@gmail.com>
2023-08-10 18:22:58 +0100

    Fixed .gitignore

--- client/src/components/modal/Modal.tsx
+++ client/src/components/modal/Modal.tsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import { ModalButton, ModalContainer, ModalContent, ModalStyles } from "./modal.styles";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Link } from "react-router-dom";
-
 
 export const Modal = () => {
 
